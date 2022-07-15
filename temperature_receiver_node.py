93bbbd521957fd25890cfb28a7b6b86f3b53fe80
Benoit Auclair <benoitauclair30@gmail.com>
2022-07-15 10:00:48 +0200

    parse string of multiple temperature measurements

--- temperature_receiver_node.py
+++ temperature_receiver_node.py
@@ -1,63 +1,21 @@
 #!/usr/bin/env python 
    
-import socket
-import time
-
-print("script started")
-
-# character separating the temperature measurements
-string_split = '//'
-
-# socket parameters of receiver
-UDP_IP = "192.168.1.147" # IP of PC
-UDP_PORT = 8888
-
-# open socket on receiver's end
-sock = socket.socket(socket.AF_INET, # Internet
-                        socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
-
-# loop until x packages are acquired just for the sake of testing
-counter = 0
-while counter != 15:
-    print("waiting for package")
-
-    # incoming data package is received
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-
-    # decode bytes into string and print to screen
-    temp_string = data.decode('UTF-8')
-    print("received message: ", temp_string)
-
-    # parse all temperature readings in string and print to screen
-    temp_reading_list = temp_string.split(string_split)
-    print("list: ", temp_reading_list)
-
-    print("type :", type(int(temp_reading_list[0])))
-
-    for reading in temp_reading_list:
-        print("reading : ", reading)
-    
-    # short pause
-    time.sleep(1)
-    counter += 1
-
-
 # #################################################################
-# file name: object_tracker_node.py
-# author's name: Diego, Benoit, Priya, Vildana
-# created on: 30-05-2022
-# last edit: 23-06-2022 (Benoit Auclair): removed all methods and variables unnecessary
-# to the task of object tracking
-# function: ROS node tracking a red blob in NAO's visual field
+# file name: temperature_receiver_node.py
+# author's name: Benoit Auclair
+# created on: 07-07-2022
+# last edit: 13-07-2022 (Benoit Auclair): added publisher
+# function: ROS node receiving temperature readings from the Arduino
 #################################################################
 
 import rospy
 from sensor_msgs.msg import Int32MultiArray
 import numpy as np
+import socket
+import time
 
 
-class ObjectTracker:
+class TemperatureReceiver:
     """
     Class implementing the node receiving temperature readings from the Arduino
     @Inputs:
@@ -69,8 +27,24 @@ class ObjectTracker:
     def __init__(self):
 
         # define frequency of execution of this node
-        self.frequency = 5.0 # Hz
+        self.frequency = 0.5 # Hz
         self.rate = rospy.Rate(self.frequency) # timing object
+
+        # socket parameters of receiver
+        self.UDP_IP = "192.168.1.147" # IP of PC
+        self.UDP_PORT = 8888
+
+        # open socket on receiver's end
+        self.sock = socket.socket(socket.AF_INET, # Internet
+                                socket.SOCK_DGRAM) # UDP
+        self.sock.bind((self.UDP_IP, self.UDP_PORT))
+        
+        # set timeout on data reception
+        self.socket_delay = 1 # seconds
+        self.sock.settimeout(self.socket_delay)
+
+        # character separating the temperature measurements
+        self.string_split = '//'
 
         # define topic publishers
         topic_temperature_readings = "/ReemC/Fingers/Temperature"
@@ -80,16 +54,45 @@ class ObjectTracker:
         self.temperature_readings_msg = None
 
 
-    def temperature_receiver(self, coordinates):
+    def temperature_receiver(self):
         """
-        Normalize the blob coordinates.
-        Inputs:
-        -array of (x,y) coordinates of an object in the visual field.
+        Handle incoming data packages transmitted over UDP protocole
+        Inputs: none
         Outputs:
-        -numpy array of normalized (x,y) coordinates.
+        -create the message to be published
         """
+        
+        print("waiting for package")
 
-        return np.array(coordinates, dtype=float) / np.array([self.cam_x_max, self.cam_y_max], dtype=float)
+        data = None
+
+        # incoming data package is received
+        try:
+            data, addr = self.sock.recvfrom(1024) # buffer size is 1024 bytes
+        except socket.timeout:
+            pass
+
+        print("timeout")
+
+        if data is not None:
+
+            self.temperature_readings_msg = Int32MultiArray()
+        
+            # decode bytes into string and print to screen
+            temp_string = data.decode('UTF-8')
+            print("received message: ", temp_string)
+
+            # parse all temperature readings in string and print to screen
+            temp_reading_list = temp_string.split(string_split)
+            print("list: ", temp_reading_list)
+
+            print("type :", type(int(temp_reading_list[0])))
+
+            for reading in temp_reading_list:
+                print("reading : ", reading)
+                self.temperature_readings_msg.append(reading)
+        
+        return 
 
 
     def run(self):
@@ -119,21 +122,22 @@ class ObjectTracker:
         -publishes the blob coordinates as Point() message.
         """
 
+        self.temperature_receiver()
+
         if self.temperature_readings_msg is not None:
 
-            # Publish blob coordinates
-            self.temperature_readings_msg.publish(self.blob_coordinates_msg)
+            # Publish temperature readings
+            self.temperature_readings_msg.publish(self.temperature_readings_msg)
             
-
 
 if __name__=='__main__':
 
     #initialize the node and set name
-    rospy.init_node('object_tracker',anonymous=True) #initilizes node
+    rospy.init_node('temperature_receiver',anonymous=True) #initilizes node
 
     # instantiate class and start loop function
     try:
-        object_tracker = ObjectTracker()
+        object_tracker = TemperatureReceiver()
         object_tracker.run()
         
     except rospy.ROSInterruptException:
